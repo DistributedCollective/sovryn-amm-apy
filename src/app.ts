fc27d90c6cd3e807aadf2f92d70a2520b135010e
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-05-31 16:34:11 +0300

    Lint

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,44 @@
-import "reflect-metadata";
-import cors from "cors";
-import express, { NextFunction, Request, Response } from "express";
-import helmet from "helmet";
-import expressRequestId from "express-request-id";
-import log from "./logger";
-import { router as ammApyRouter } from "./routes/ammApy.route";
-import { helloSovrynHandler } from "./controllers/helloSovrynHandler";
-import { toTheMoonHandler } from "./controllers/toTheMoonHandler";
-import asyncMiddleware from "./utils/asyncMiddleware";
-import responseTime from "response-time";
-import errorHandler from "./errorHandlers/errorHandler";
-import { HTTP404Error } from "./errorHandlers/baseError";
+import 'reflect-metadata'
+import cors from 'cors'
+import express, { NextFunction, Request, Response } from 'express'
+import helmet from 'helmet'
+import expressRequestId from 'express-request-id'
+import log from './logger'
+import { router as ammApyRouter } from './routes/ammApy.route'
+import responseTime from 'response-time'
+import errorHandler from './errorHandlers/errorHandler'
+import { HTTP404Error } from './errorHandlers/baseError'
 
-const app = express();
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(responseTime());
-app.use(expressRequestId());
-app.use(log);
+const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(responseTime())
+app.use(expressRequestId())
+app.use(log)
 
 app.use((req: Request, _res: Response, next: NextFunction) => {
   // TODO: printing req body for debug purposes
-  req.log.info(req, `Incoming ${req.method} request to ${req.path}`);
-  next();
-});
+  req.log.info(req, `Incoming ${req.method} request to ${req.path}`)
+  next()
+})
 
-app.get("/", (req, res) => {
-  req.log.info(req, "Sovryn Amm Apy Service Running. Stay Sovryn.");
-  res.send("Sovryn Amm Apy Service Running. Stay Sovryn.");
-});
+app.get('/', (req, res) => {
+  req.log.info(req, 'Sovryn Amm Apy Service Running. Stay Sovryn.')
+  res.send('Sovryn Amm Apy Service Running. Stay Sovryn.')
+})
 
-app.use("/amm", ammApyRouter);
+app.use('/amm', ammApyRouter)
 
 app.use(function (_req: Request, res: Response, _next: NextFunction) {
-  res.status(404).send("Sorry can't find that!");
-});
+  res.status(404).send("Sorry can't find that!")
+})
 
 app.use(function (_req: Request, _res: Response, _next: NextFunction) {
-  throw new HTTP404Error();
-});
+  throw new HTTP404Error()
+})
 
-app.use(errorHandler.handleError);
+app.use(errorHandler.handleError)
 
-export default app;
+export default app
