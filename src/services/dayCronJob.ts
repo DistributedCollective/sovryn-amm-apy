9a3254355040b2c46c6fc9e63d9641b7cff362ea
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-06-21 18:48:25 +0400

    Added tests

--- src/services/dayCronJob.ts
+++ src/services/dayCronJob.ts
@@ -12,18 +12,18 @@ const logger = log.logger.child({ module: 'Apy Day Cronjob' })
 export async function main (): Promise<void> {
   const yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000)
   const rawDayData = await getDailyAggregatedApy(yesterday.toISOString())
+  logger.debug(rawDayData)
   for (const row of rawDayData) {
     if (parseFloat(row.avg_balance) > 0) {
-      console.log(row)
       const calculatedDayData = calculateDayApr(row)
-      console.log(calculatedDayData)
+      logger.debug(calculatedDayData)
       await saveApyDayRow(calculatedDayData)
     }
   }
   logger.info('All amm apy day data rows saved')
 }
 
-function calculateDayApr (data: IRawDayData): ICalculatedDayData {
+export function calculateDayApr (data: IRawDayData): ICalculatedDayData {
   const feeApy = bignumber(data.sum_fees)
     .div(bignumber(data.avg_balance))
     .mul(365 * 100)
