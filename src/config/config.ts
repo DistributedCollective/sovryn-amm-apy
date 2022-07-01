783cddce6f1e9bc96e99a91695bd675ed35cc7f6
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-07-01 19:27:13 +0400

    Lint

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,63 +1,63 @@
-import convict from "convict";
+import convict from 'convict'
 
 export enum Environment {
-  Production = "production",
-  Development = "development",
-  Test = "test",
+  Production = 'production',
+  Development = 'development',
+  Test = 'test',
 }
 
 const config = convict({
   env: {
-    doc: "The application environment.",
+    doc: 'The application environment.',
     format: [...Object.values(Environment)],
-    default: "development",
-    env: "NODE_ENV",
+    default: 'development',
+    env: 'NODE_ENV'
   },
   port: {
-    doc: "The port to bind.",
-    format: "port",
+    doc: 'The port to bind.',
+    format: 'port',
     default: 3000,
-    env: "PORT",
+    env: 'PORT'
   },
   appName: {
-    doc: "application name",
+    doc: 'application name',
     format: String,
-    default: "sovryn-amm-apy",
-    env: "APP_NAME",
+    default: 'sovryn-amm-apy',
+    env: 'APP_NAME'
   },
   logLevel: {
-    doc: "application log level",
-    format: ["trace", "debug", "info", "warn", "error", "fatal"],
-    default: "info",
-    env: "LOG_LEVEL",
+    doc: 'application log level',
+    format: ['trace', 'debug', 'info', 'warn', 'error', 'fatal'],
+    default: 'info',
+    env: 'LOG_LEVEL'
   },
   postgresHost: {
-    doc: "postgres host",
+    doc: 'postgres host',
     format: String,
-    default: "sov-postgres",
-    env: "POSTGRES_HOST",
+    default: 'sov-postgres',
+    env: 'POSTGRES_HOST'
   },
   postgresPort: {
-    doc: "postgres port",
-    format: "port",
+    doc: 'postgres port',
+    format: 'port',
     default: 5432,
-    env: "POSTGRES_PORT",
+    env: 'POSTGRES_PORT'
   },
   postgresUser: {
-    doc: "postgres user",
+    doc: 'postgres user',
     format: String,
-    default: "sov-postgres",
-    env: "POSTGRES_USER",
+    default: 'sov-postgres',
+    env: 'POSTGRES_USER'
   },
   postgresPassword: {
-    doc: "postgres password",
-    format: "*",
-    default: "",
-    env: "POSTGRES_PASSWORD",
-    sensitive: true,
+    doc: 'postgres password',
+    format: '*',
+    default: '',
+    env: 'POSTGRES_PASSWORD',
+    sensitive: true
   },
   postgresDatabase: {
-    doc: "postgres database",
+    doc: 'postgres database',
     format: String,
     default: 'sov-amm-apy',
     env: 'POSTGRES_DB'
@@ -101,6 +101,6 @@ const config = convict({
   }
 })
 
-config.validate({ allowed: "strict" });
+config.validate({ allowed: 'strict' })
 
-export default config.getProperties();
+export default config.getProperties()
