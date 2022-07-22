8e70eab78d98e4a725f58bc0faaef788bb9462fd
BetsyBraddock <69518422+BetsyBraddock@users.noreply.github.com>
2022-07-22 16:10:51 +0100

    Improved sql queries so data is chronological (#14)

--- src/models/apyDay.model.ts
+++ src/models/apyDay.model.ts
@@ -1,5 +1,5 @@
 import { ICalculatedDayData } from './apyBlock.model'
-import { getRepository, MoreThan } from 'typeorm'
+import { getRepository, MoreThan, MoreThanOrEqual } from 'typeorm'
 import { ApyDay } from '../entity'
 import { isNil } from 'lodash'
 import { HTTP404Error } from '../errorHandlers/baseError'
@@ -52,15 +52,23 @@ export async function getAllPoolData (days: number): Promise<ApyDay[]> {
       'rewardsApy',
       'feeApy',
       'totalApy'
-    ]
+    ],
+    order: { date: 'ASC' }
   })
   return result
 }
 
+/** This function first gets the max date in the apy day table, and then returns the apy for that pool on that day
+ * Why doesn't it just return data for today? Because if a query is made around midnight but before the cron job has run, the api would return nothing
+ */
 export async function getOnePoolApy (pool: string): Promise<ApyDay[]> {
   const repository = getRepository(ApyDay)
+  const maxDate = await repository
+    .createQueryBuilder('date')
+    .select('MAX(date.date)', 'max')
+    .getRawOne()
   const result = await repository.find({
-    where: { pool: pool },
+    where: { pool: pool, date: MoreThanOrEqual(maxDate.max) },
     select: ['pool', 'poolToken', 'date', 'rewardsApy', 'feeApy', 'totalApy']
   })
   if (result.length > 0) {
@@ -87,7 +95,8 @@ export async function getOnePoolData (
       'rewardsApy',
       'feeApy',
       'totalApy'
-    ]
+    ],
+    order: { date: 'ASC' }
   })
   if (result.length > 0) {
     return result
